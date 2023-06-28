f036feb750f2eb5b0a5c5903e13669b901db9762
NIDIABUG <nidiastudioph@gmail.com>
2023-06-29 06:04:26 +0800

    added Data Structures

---
+++ src/arrays-objects.js
@@ -0,0 +1,41 @@
+const simpleArray = [1, 2, 3, 4, 5];
+const simpleObject = {
+    name: 'John',
+    age: 20
+};
+
+// Arrays inside an array
+// const nestedArray = [[10, 20], [30, 40]];
+const nestedArray = [
+    [10, 20],
+    [30, 40]
+];
+// Objects inside an Object
+// const nestedObject = {personOne: {name: 'Albedo', age: 100}, personTwo: {name: 'Ainzel', age: 34}};
+const nestedObject = {
+    personOne: {
+        name: 'Albedo',
+        age: 100
+    },
+    personTwo: {
+        name: 'Ainzel',
+        age: 34
+    }
+};
+
+// Objects inside an Array
+// const arrayOfObj = [{name: 'Yuan', age: 36}, {name: 'Boragen', age: 69}];
+const arrayOfObj = [
+    {name: 'Yuan', age: 36},
+    {name: 'Boragen', age: 69}
+];
+// Arrays inside an Object
+// const objOfArray = {arrayOne: [1, 2, 3, 4, 5], arrayTwo: ['one', 'two', 'three', 'four', 'five']};
+const objOfArray = {
+    arrayOne: [
+        1, 2, 3, 4, 5
+    ],
+    arrayTwo: [
+        'one', 'two', 'three', 'four', 'five'
+    ]
+};
